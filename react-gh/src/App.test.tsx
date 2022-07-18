d7ac42f83969b7bfc07df70eab9202338ad4d3d8
aslik97 <61617567+aslik97@users.noreply.github.com>
2022-07-19 00:33:04 +0200

    test

--- react-gh/src/App.test.tsx
+++ react-gh/src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { App } from './App';
 
-it('renders "Welcome to the Enterprise-Scale Landing Zone Generator"', () => {
+it('renders "Welcome to the Enterprise-Scale Landing Zone Generator test"', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Welcome to the Enterprise-Scale Landing Zone Generator/i);
+  const linkElement = screen.getByText(/Welcome to the Enterprise-Scale Landing Zone Generator test/i);
   expect(linkElement).toBeInTheDocument();
 });
